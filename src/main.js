58f0d86b26f5db93f68ef3cc59feed3468c7a13b
tamas.kuti <tamas.kuti@allaboutcake.com>
2021-06-22 10:17:45 +0300

    Remove comments

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import Vue from 'vue'
-// import { IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './infrastructure/style/global.css'
 import App from './application/App.vue'
-
-// Vue.use(BootstrapVue)
-// Vue.use(IconsPlugin)
 
 new Vue({
     el: '#app',
