68ed862699993cd9b5d8fe2693ac535dbbfd6d04
tamas.kuti <tamas.kuti@allaboutcake.com>
2021-08-03 15:36:46 +0200

    Add docs and comments on deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import legacy from '@vitejs/plugin-legacy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    // In production the `base` has to be an absolute URL of the GitHub page, where we're deploying to
+    // otherwise it will throw a 404 and the app won't start.
+    // The BASE_URL should be set on the CI (see .github/workflows/main.yml)
     base: process.env.BASE_URL ? process.env.BASE_URL : '/',
     plugins: [
         createVuePlugin(),
